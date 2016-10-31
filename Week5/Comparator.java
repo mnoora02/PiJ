cd244e73e7472c4d77df8e039d636b2ffdc2a58c
mo <mo.noorani@icloud.com>
2016-10-31 14:00:43 +0000

    work in progress...

---
+++ Week5/Comparator.java
@@ -0,0 +1,25 @@
+public class Comparator {
+	public int getMax(int n, int m) {
+		if (n > m) {
+			return n;
+		} else {
+			return m;
+		}
+	}
+	public double getMax(double d1, double d2) {
+		if (d1 > d2) {
+			return d1;
+		} else {
+			return d2;
+		}
+	}
+	public String getMax(String number1, String number2) {
+		int n1 = Integer.parseInt(number1);
+		int n2 = Integer.parseInt(number2);
+		if (n1 > n2) {
+			return number1;
+		} else {
+			return number2;
+		}
+	}
+}
