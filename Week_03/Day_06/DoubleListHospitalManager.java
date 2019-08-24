0f109ab89df88bb8be90addfe1fb9fd3d317489d
mnoora02 <mnoora02@dcs.bbk.ac.uk>
2019-08-24 16:06:44 +0100

    code/comments updated - filename needs changing

--- Week_03/Day_06/DoubleListHospitalManager.java
+++ Week_03/Day_06/DoubleListHospitalManager.java
@@ -1,178 +1,119 @@
-package Week_03.Day_06;
+package Day_06;
 
-public class DoubleListHospitalManager {
-	
-	// private Patient[] patientArray = new Patient[90];
-	private DoubleListPatient firstPatient = null;
+public class Q3_DblListHospitalMngr {
+    // private Patient[] patientArray = new Patient[90];
+    private Q3_DblListPatient firstPatient = null;
 
-	public void addPatient(DoubleListPatient newPatient) {
-		if (firstPatient == null) {							// if list is empty
-			firstPatient = newPatient; 						// set fP. to nP. - this doesn't change from signly linked list
-		}
-		else {
-			DoubleListPatient mockPatient = firstPatient;	// set mP. to fP
-			while (mockPatient.getNextPatient() != null) {	// while mP.gNP !=null
-				mockPatient = mockPatient.getNextPatient();	// mP. becomes the mP.gNP.
-			}
-			mockPatient.setNextPatient(newPatient);			// set mockP's nextP. to be the newPatient by passing newPatient as a PARAM 
-			newPatient.setPrevPatient(mockPatient);			// set newPatient's prev to be mockP by passing mockP. as a PARAM 
-		}													// NB: (need to create setter for setPrevPatient)
-	}
-	
-	public boolean deletePatient(String name) {
-		if (firstPatient == null) {							// if list is empty
-			return false; 									// return false
-		}
-		else if (firstPatient.getName().equals(name)) {		// if fP. name is a match - we need to delete
-			firstPatient = firstPatient.getNextPatient(); 	// fP. becomes our fP.gNP
-			firstPatient.setPrevPatient(null);				// what was our second index pos. is now our fP. - we need to set prev pointer to null
-			return true;									// return true
-		}
-		else {
-			DoubleListPatient mockPatient = firstPatient;										// set mP. to our fP.
-			while (mockPatient.getNextPatient() != null) {										// whilst our mP.gNP != null
-				if (mockPatient.getNextPatient().getName().equals(name)) {						// if mP.gNP is a match
-					mockPatient.setNextPatient(mockPatient.getNextPatient().getNextPatient());	// we set mP pointer to mP.gNP.gNP 
-					mockPatient.getNextPatient().setPrevPatient(mockPatient);					// we use line above to set its prev pointer to mP.
-					return true;																// return true
-				}
-				mockPatient = mockPatient.getNextPatient();										// if name not found we move to next position
-			}
-			return false;																		// if name not in list we return false
-		}
-	}
+    public void addPatient(Q3_DblListPatient newPatient) {
+        // if list is empty
+        if (firstPatient == null) {
+            // set fP. to nP. - this doesn't change from singly linked list
+            firstPatient = newPatient;
+        }
+        else {
+            // set mP. to fP
+            Q3_DblListPatient currentPatient = firstPatient;
+            // while mP.gNP !=null
+            while (currentPatient.getNextPatient() != null) {
+                // mP. becomes the mP.gNP.
+                currentPatient = currentPatient.getNextPatient();
+            }
+            // set mockP's nextP. to be the newPatient by passing newPatient as a PARAM
+            currentPatient.setNextPatient(newPatient);
+            // set newPatient's prev to be mockP by passing mockP. as a PARAM
+            newPatient.setPrevPatient(currentPatient);
+            // NB: (need to create setter for setPrevPatient in DblListPatient class)
+        }
+    }
 
-	public void toPrint() {									// method will work for singly & doubly linked lists
-		if (firstPatient == null) {
-			System.out.println("No patients in list!");
-		}
-		else {
-			DoubleListPatient mockPatient = firstPatient;
-			while (mockPatient != null) {
-				System.out.println(mockPatient);
-				mockPatient = mockPatient.getNextPatient();
-			}
-		}
-	}
+    public boolean deletePatient(String name) {
+        // if list is empty return false
+        if (firstPatient == null) {
+            return false;
+        }
+        // if fP. name is a match - we need to delete
+        else if (firstPatient.getName().equals(name)) {
+            // fP. becomes our fP.gNP
+            firstPatient = firstPatient.getNextPatient();
+            // what was our second index pos. is now our fP. - we need to set prev pointer to null
+            firstPatient.setPrevPatient(null);
+            // return true
+            return true;
+        }
+        else {
+            // set mP. to our fP.
+            Q3_DblListPatient currentPatient = firstPatient;
+            // whilst our mP.gNP != null
+            while (currentPatient.getNextPatient() != null) {
+                // if mP.gNP is a match
+                if (currentPatient.getNextPatient().getName().equals(name)) {
+                    // we set mP pointer to mP.gNP.gNP
+                    currentPatient.setNextPatient(currentPatient.getNextPatient().getNextPatient());
+                    // we use line above to set its prev pointer to mP.
+                    currentPatient.getNextPatient().setPrevPatient(currentPatient);
+                    // return true
+                    return true;
+                }
+                // if name not found we move to next position
+                currentPatient = currentPatient.getNextPatient();
+            }
+            // if name not in list we return false
+            return false;
+        }
+    }
 
-	public void printBackwards() {							// print backwards - go through list first - then come back and print along the way
-		if (firstPatient == null) {							// if list is null
-			System.out.println("No patients in list!");		// do this 
-		}
-		else {
-			DoubleListPatient mockPatient = firstPatient;	// create mP and set it to fP
-			while (mockPatient.getNextPatient() != null) {	// whilst mP.gNP != null iterate through list and stop on last pos.
-				mockPatient = mockPatient.getNextPatient();	// mP brcomes mP. gNP90
-			}
-			while (mockPatient != null) {					// now we're at last pos. & whilst mP != null because we still want to print last index pos
-				System.out.println(mockPatient);			// print mP. 
-				mockPatient = mockPatient.getPrevPatient();	// mP becomes mP.getPrevP (now we need to create a getPrevPatient() getter)
-			}
-		}
-	}
+    // method will work for singly & doubly linked lists
+    public void toPrint() {
+        if (firstPatient == null) {
+            System.out.println("No patients in list!");
+        }
+        else {
+            Q3_DblListPatient currentPatient = firstPatient;
+            while (currentPatient != null) {
+                System.out.println(currentPatient);
+                currentPatient = currentPatient.getNextPatient();
+            }
+        }
+    }
 
-	public int lengthList() {								// method will work for singly & doubly linked lists
-		//if (firstPatient == null) {
-		//	return 0;
-		//}
-		DoubleListPatient mockPatient = firstPatient; 
-		int counter = 0;
-		while (mockPatient != null) {
-			counter ++; 
-			mockPatient = mockPatient.getNextPatient(); 
-		}
-		return counter;
-	}
+    // print backwards - go through list first - then come back and print along the way
+    public void printBackwards() {
+        // if list is null - do this
+        if (firstPatient == null) {
+            System.out.println("No patients in list!");
+        }
+        else {
+            // create mP and set it to fP
+            Q3_DblListPatient currentPatient = firstPatient;
+            // whilst mP.gNP != null iterate through list and stop on last pos.
+            while (currentPatient.getNextPatient() != null) {
+                // mP brcomes mP. gNP90
+                currentPatient = currentPatient.getNextPatient();
+            }
+            // now we're at last pos. & whilst mP != null because we still want to print last index pos
+            while (currentPatient != null) {
+                // print mP.
+                System.out.println(currentPatient);
+                // mP becomes mP.getPrevP (now we need to create a getPrevPatient() getter)
+                currentPatient = currentPatient.getPrevPatient();
+            }
+        }
+    }
+
+    // method will work for singly & doubly linked lists
+    public int lengthList() {
+        //if (firstPatient == null) {
+        //	return 0;
+        //}
+        Q3_DblListPatient currentPatient = firstPatient;
+        int counter = 0;
+        while (currentPatient != null) {
+            counter ++;
+            currentPatient = currentPatient.getNextPatient();
+        }
+        return counter;
+    }
 } // END of class
 
-// NB: it is only the adding and the deleting methods we wil need to adjust re: prevPointer 
+// NB: it is only the adding and the deleting methods we wil need to adjust re: prevPointer
 // NB: toPrint() & lengthList() methods will work for both singly & doubly linked lists
-
-/**
-code minus the comments: 
-
-public class DoubleListHospitalManager {
-	
-	// private Patient[] patientArray = new Patient[90];
-	private DoubleListPatient firstPatient = null;
-
-	public void addPatient(DoubleListPatient newPatient) {
-		if (firstPatient == null) {
-			firstPatient = newPatient; 
-		}
-		else {
-			DoubleListPatient mockPatient = firstPatient;
-			while (mockPatient.getNextPatient() != null) {
-				mockPatient = mockPatient.getNextPatient();
-			}
-			mockPatient.setNextPatient(newPatient);
-			newPatient.setPrevPatient(mockPatient);
-		}
-	}
-	
-	public boolean deletePatient(String name) {
-		if (firstPatient == null) {
-			return false; 
-		}
-		else if (firstPatient.getName().equals(name)) {
-			firstPatient = firstPatient.getNextPatient(); 
-			firstPatient.setPrevPatient(null);
-			return true;
-		}
-		else {
-			DoubleListPatient mockPatient = firstPatient;
-			while (mockPatient.getNextPatient() != null) {
-				if (mockPatient.getNextPatient().getName().equals(name)) {
-					mockPatient.setNextPatient(mockPatient.getNextPatient().getNextPatient());
-					mockPatient.getNextPatient().setPrevPatient(mockPatient);
-					return true;
-				}
-				mockPatient = mockPatient.getNextPatient();
-			}
-			return false;
-		}
-	}
-
-	public void toPrint() {
-		if (firstPatient == null) {
-			System.out.println("No patients in list!");
-		}
-		else {
-			DoubleListPatient mockPatient = firstPatient;
-			while (mockPatient != null) {
-				System.out.println(mockPatient);
-				mockPatient = mockPatient.getNextPatient();
-			}
-		}
-	}
-
-	public void printBackwards() {
-		if (firstPatient == null) {
-			System.out.println("No patients in list!");
-		}
-		else {
-			DoubleListPatient mockPatient = firstPatient;
-			while (mockPatient.getNextPatient() != null) {
-				mockPatient = mockPatient.getNextPatient();
-			}
-			while (mockPatient != null) {
-				System.out.println(mockPatient);
-				mockPatient = mockPatient.getPrevPatient();
-			}
-		}
-	}
-
-	public int lengthList() {
-		//if (firstPatient == null) {
-		//	return 0;
-		//}
-		DoubleListPatient mockPatient = firstPatient; 
-		int counter = 0;
-		while (mockPatient != null) {
-			counter ++; 
-			mockPatient = mockPatient.getNextPatient(); 
-		}
-		return counter;
-	}
-} // END of class
-*/
