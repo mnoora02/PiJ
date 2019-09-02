3bc720ae755820a4cd1f1beed5f950102f820587
mnoora02 <mnoora02@dcs.bbk.ac.uk>
2019-09-02 12:39:05 +0100

    updated code & comments

--- Week_04/Updated_Day_08/Q7/StringExtension.java
+++ Week_04/Updated_Day_08/Q7/StringExtension.java
@@ -1,5 +1,26 @@
-package Week_04.Updated_Day_08.Q7;
+package Day_08.Q7;
 
-public class StringExtension extends String {
-	
-}
+/**
+ * Q7.1 Final means no change
+ * Create a class that extends String and adds a method printEven() that prints on screen
+ * the even-numbered characters of the string.
+ *
+ * Try to compile it and see what happens.
+ *
+ * Ans: you cannot extend class String because in the java docs we see that class String is declared as:
+ *          public final class String()
+ *
+ * meaning that you cannot extend class String
+ *
+ * if make a class final, then you cannot extend it
+ * if you make a method final, you cannot override it
+ * if you make a field final, you cannot change it
+ *
+ * however, using reflection we can override a class, its accessors, mutability etc.
+ * - really powerful & potentially dangerous
+ *
+ */
+
+// commented out - alternative is exclude entire folder
+//public class StringExtension extends String {
+//}
--- Week_04/Updated_Day_08/Q7/StringExtensionMainApp.java
+++ Week_04/Updated_Day_08/Q7/StringExtensionMainApp.java
@@ -1,13 +1,45 @@
-package Week_04.Updated_Day_08.Q7;
+package Day_08.Q7;
 
 import java.util.Arrays;
 
+/**
+ * Q7.2
+ *
+ * create our main app
+ * we need to obtain the class String
+ * lets create a string and get the class on/of that string
+ *
+ * create an object of type Class, call the object something
+ * get the class of our string i.e. call .getClass()
+ *
+ * we now have a class object and we can get any information on it and from it:
+ * - its name
+ * - its interface
+ * - its constructors
+ * - get any declared fields
+ * - get declared methods - gives you an array of methods - within each of those methods you could invoke/alter it
+ * - get declared fields
+ *
+ * // print the Array of objects
+ * // call .toString() to stringify the Array
+ * // in parantheses call stringClass.getDeclaredMethods()
+ * System.out.println(Arrays
+ *                 .toString(stringClass.getDeclaredMethods()));
+ * // you'll need to import.java.util.Arrays
+ *
+ *
+ */
+
 public class StringExtensionMainApp {
 
-	public static void main(String[] args) {
-		String s = "";
-		Class stringClass = s.getClass();
-		System.out.println(Arrays.toString(stringClass.getDeclaredMethods()));
-	}
+    public static void main(String[] args) {
+        String s = "";
+        Class stringClass = s.getClass();
+        System.out.println(Arrays
+                .toString(stringClass.getDeclaredMethods()));
+        System.out.println("");
+        System.out.println(Arrays
+                .toString(stringClass.getDeclaredFields()));
+    }
 
 }
