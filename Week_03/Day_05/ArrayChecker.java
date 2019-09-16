5143472a7cf1c69cadb2a0b5051d52ab15261be5
mnoora02 <mnoora02@dcs.bbk.ac.uk>
2019-09-16 18:01:05 +0100

    minor change to variable names in reverse method
    outputIndexPos changed to indexPos
    outputLastIndexPos changed to lastIndexPos

--- Week_03/Day_05/ArrayChecker.java
+++ Week_03/Day_05/ArrayChecker.java
@@ -1,10 +1,10 @@
-package Day05;
+package Day_05;
 
 /*
 Question:
 2 Checking arrays
 Create a new Java class called Q2_ArrayChecker with two methods:
-isSymetrical(int[ ): ] a method that returns true if the array of int provided as argument is symmetrical and
+isSymmetrical(int[ ): ] a method that returns true if the array of int provided as argument is symmetrical and
 false otherwise.
 reverse(int[ ): ] a method that takes an array of int and returns another array of int of the same size and with
 the same numbers, but in opposite order.
@@ -28,26 +28,28 @@ public class Q2_ArrayChecker {
 
     // create a launch method IOT to call it within the PSVM
     public void launch() {
-        // create a symm. int[] and instantiate with {} IOT to test our methods below
+        // create a symmetrical int[] and instantiate with {} IOT to test our methods below
         int[] numArray = {12, 14, 16, 111, 16, 14, 12};
         // print with method plus a param for the method also
-        System.out.println(isSymmetrical(numArray));
+        System.out.println("numArray [12, 14, 16, 111, 16, 14, 12] is symmetrical: " + isSymmetrical(numArray));
         System.out.println();
 
-        // create an asymm. int[] and instantiate with {} IOT to test our methods below
+        // create an asymmetrical int[] and instantiate with {} IOT to test our methods below
         int[] numArray2 = {120, 130, 140, 150, 160, 170, 180, 190};
-        System.out.println(isSymmetrical(numArray2));
+        System.out.println("numArray2 [120, 130, 140, 150, 160, 170, 180, 190] is symmetrical: " + isSymmetrical(numArray2));
         System.out.println();
 
         // run reverse method on first array BUT NB: turn into String - otherwise we'll get the memory address location
         // NB. in Java ArrayLists need to be turned into Arrays then turned into Strings - we already have Arrays so
-        // we just need to Stringify them - and that is all plus print it out of course
-        System.out.println(Arrays.toString(reverse(numArray)));
+        // we just need to Stringify them - and that is all and print it out of course
+        System.out.println(
+                "reverse numArray \n[12, 14, 16, 111, 16, 14, 12] : \n" + Arrays.toString(reverse(numArray)));
         //System.out.println(reverse(numArray).toString());
         System.out.println();
 
-        // run reverse method on second array BUT NB: turn into String
-        System.out.println(Arrays.toString(reverse(numArray2)));
+         // run reverse method on second array BUT NB: turn into String
+        System.out.println(
+                "reverse numArray2 \n[120, 130, 140, 150, 160, 170, 180, 190] : \n" + Arrays.toString(reverse(numArray2)));
         // System.out.println(reverse(numArray2).toString());
     }
 
@@ -84,6 +86,10 @@ public class Q2_ArrayChecker {
         return true;
     }
 
+//    public int extractMidValue(int[] valuesArray) {
+//        int
+//    }
+
     // initialise a reverse method that takes in an array, called array, as a PARAM
     public int[] reverse(int[] array) {
         // initialise a var, sizeArray, to the length of array
@@ -95,21 +101,21 @@ public class Q2_ArrayChecker {
         }
 
         // initialise the output index pos. to 0
-        int outputIndexPos = 0;
+        int indexPos = 0;
         // create var lastIndexPos and initialise to end of the array-1
-        int outputLastIndexPos = sizeArray-1;
+        int lastIndexPos = sizeArray-1;
 
         // create an array (initialise an array object), called output, and it will be
         // equal in size to the array (sizeArray) i.e. size of output = size of input
         int[] output = new int[sizeArray];
 
         // we want to stop when the lastIndexPos is equal to the 0th pos. until then...
-        while (outputLastIndexPos >= 0) {
+        while (lastIndexPos >= 0) {
             // value in the index pos. in output = value held in last index pos. in the array
-            output[outputIndexPos] = array[outputLastIndexPos];
+            output[indexPos] = array[lastIndexPos];
             // increment the output array index pos. and decrement the array's last index pos.
-            outputIndexPos++;
-            outputLastIndexPos--;
+            indexPos++;
+            lastIndexPos--;
             // once all values copied - loop ends
         } // END while loop
         // return the output array
