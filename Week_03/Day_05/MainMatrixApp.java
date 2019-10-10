3f57c04e9402fc87a9002b64f92cf93d3baaf8cb
mnoora02 <mnoora02@dcs.bbk.ac.uk>
2019-10-10 18:14:37 +0100

    incorporates setMatrix() method for Q5* - works

--- Week_03/Day_05/MainMatrixApp.java
+++ Week_03/Day_05/MainMatrixApp.java
@@ -1,6 +1,4 @@
-// this MainApp file was previously called QuestionFourMain.java
-
-package Day05;
+package Day_05;
 
 public class Q4_MatrixMainApp {
 
@@ -10,14 +8,19 @@ public class Q4_MatrixMainApp {
         System.out.println("Base matrix only\n" + runMatrix + "\n");
 
         runMatrix.setElement(3, 2, 7);
-        System.out.println("Set elem. pos1/index_row = 3, pos2/index_column = 2, value = 7\n" + runMatrix + "\n");
+        System.out.println("Set elem. \npos1/index_row = 3 \npos2/index_column = 2 \nvalue = 7\n" + runMatrix + "\n");
 
         runMatrix.setRow(1, "9,8,7,6");
-        System.out.println("Set row: 1\n" + runMatrix + "\n");
+        System.out.println("Set row: 1 \nvalues = 9, 8, 7, 6 \n" + runMatrix + "\n");
 
         runMatrix.setColumn(0, "34,56,78,89");
-        System.out.println("Set column: 0\n" + runMatrix + "\n");
+        System.out.println("Set column: 0 \nvalues = 34, 56, 78, 89 \n" + runMatrix + "\n");
 
         runMatrix.toPretty();
+        System.out.println(" ");
+
+        Q4_Matrix runMatrix2 = new Q4_Matrix(3, 3);
+        runMatrix2.setMatrix("1, 2, 3; 4, 5, 6; 7, 8, 9");
+        runMatrix2.toPretty();
     }
 }
